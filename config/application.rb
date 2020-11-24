71b466b99e68cdde039fc9667546cde9033d72b2
Light871 <light872@yandex.ru>
2020-11-24 15:43:20 +0300

    FIX  Change default template format erb --> haml

--- config/application.rb
+++ config/application.rb
@@ -11,13 +11,13 @@ module Flashcards
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.generators do |g|
+	  g.template_engine :haml
+	end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
-end
-
-config.generators do |g|
-  g.template_engine :haml
 end
