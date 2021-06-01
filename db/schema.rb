4e9cddd87ced9a81c62d4d67af424fb6f05962d3
Light871 <light872@yandex.ru>
2021-06-01 17:57:00 +0300

    rake db:rollback

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_528_075_100) do
+ActiveRecord::Schema.define(version: 0) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "cards", force: :cascade do |t|
-    t.string "original_text"
-    t.string "translated_text"
-    t.date "review_date"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 end
