0c5665d8d29796cdc423233ed9f6d6d7edf87b92
Light871 <light872@yandex.ru>
2021-06-24 07:04:51 +0300

    Add callback 'before_create' to model 'card' for auto add 'review_date'

--- app/models/card.rb
+++ app/models/card.rb
@@ -4,6 +4,10 @@ class Card < ApplicationRecord
 
   validate :original_text_cannot_be_equal_to_translated_text
 
+  before_create do
+    self.review_date = Date.today + 3
+  end
+
   def original_text_cannot_be_equal_to_translated_text
     errors.add(:equally, "can't be equal") if
       original_text.downcase == translated_text.downcase
