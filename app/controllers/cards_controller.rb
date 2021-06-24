4883db15a4aa5b5a26e997baeed090385c6c1adc
Light871 <light872@yandex.ru>
2021-06-24 06:49:15 +0300

    Implement cards#card_params

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,4 +2,10 @@ class CardsController < ApplicationController
   def index
     @cards = Card.all
   end
+
+  private
+
+  def card_params
+    params.require(:card).permit(:original_text, :translated_text)
+  end
 end
