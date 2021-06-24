a8e6d6dbc21d5a6aad6cd4e01394fbf1bd208229
Light871 <light872@yandex.ru>
2021-06-24 07:03:35 +0300

    Add validation method for equal values 'original_text' and 'translated_text'

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,11 @@
 class Card < ApplicationRecord
   validates :original_text, presence: true
   validates :translated_text, presence: true
+
+  validate :original_text_cannot_be_equal_to_translated_text
+
+  def original_text_cannot_be_equal_to_translated_text
+    errors.add(:equally, "can't be equal") if
+      original_text.downcase == translated_text.downcase
+  end
 end
