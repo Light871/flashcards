d8cd148872886dd903f96551e117a1ecd5a3e454
Light871 <light872@yandex.ru>
2021-06-24 06:51:50 +0300

    Implement card#create

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -6,6 +6,16 @@ class CardsController < ApplicationController
   def new
   end
 
+  def create
+    @card = Card.new(card_params)
+
+    if @card.save
+      redirect_to @card
+    else
+      render action: 'new'
+    end
+  end
+
   private
 
   def card_params
