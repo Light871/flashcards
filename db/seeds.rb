b9c2572174af225586a579ada3b244009f7599fe
Light871 <light872@yandex.ru>
2021-06-29 03:25:13 +0300

    seeds.rb small fixes #2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@ require 'open-uri'
 page = Nokogiri::HTML(open("http://1000mostcommonwords.com/1000-most-common-german-words/"))
 
 2.upto(1001) do |n|
-  Card.create(original_text: page.xpath("//*[@id=\"post-115\"]/div/div/table/tbody/tr[#{n}]/td[2]").inner_html, 
+  Card.create(original_text: page.xpath("//*[@id=\"post-115\"]/div/div/table/tbody/tr[#{n}]/td[2]").inner_html,
               translated_text: page.xpath("//*[@id=\"post-115\"]/div/div/table/tbody/tr[#{n}]/td[3]").inner_html)
 end
