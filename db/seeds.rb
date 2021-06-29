20b2006483284a3d7a789e659f31cf2ad7774cb0
Light871 <light872@yandex.ru>
2021-06-29 03:23:47 +0300

    seeds.rb small fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
-   
-page = Nokogiri::HTML(open("http://1000mostcommonwords.com/1000-most-common-german-words/"))   
+
+page = Nokogiri::HTML(open("http://1000mostcommonwords.com/1000-most-common-german-words/"))
 
 2.upto(1001) do |n|
   Card.create(original_text: page.xpath("//*[@id=\"post-115\"]/div/div/table/tbody/tr[#{n}]/td[2]").inner_html, 
