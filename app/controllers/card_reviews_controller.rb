9c7b4fae80317384ed84e0389a2ba689f217d11a
Light871 <light872@yandex.ru>
2021-07-26 11:04:43 +0300

    Implement user's translate check

--- app/controllers/card_reviews_controller.rb
+++ app/controllers/card_reviews_controller.rb
@@ -3,6 +3,17 @@ class CardReviewsController < ApplicationController
     @card = Card.review_ready.first
   end
 
+  def create
+    @card = Card.find(params[:card_id])
+    if @card.translated_text.casecmp?(card_params[:translated_text])
+      @card.update(review_date: Date.today + 3)
+      flash[:notice] = "Правильно!"
+    else
+      flash[:notice] = "Неправильно! Попробуй еще одну!"
+    end
+    redirect_to root_path
+  end
+
   private
 
   def card_params
