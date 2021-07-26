1d9b6abe36c78ea89523e1de3ecef54507751532
Light871 <light872@yandex.ru>
2021-07-26 11:16:16 +0300

    Small style fixes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'card_reviews#new'
 
   resources :cards do
-    resources :card_reviews, only: [:new, :create]
+    resources :card_reviews, only: %i[new create]
   end
 end
