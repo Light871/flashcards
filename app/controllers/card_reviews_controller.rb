d771d41fa3c3c296003d32263e86c5e7dbee2488
Light871 <light872@yandex.ru>
2021-07-26 11:03:26 +0300

    Implement cardreviews#card_params

--- app/controllers/card_reviews_controller.rb
+++ app/controllers/card_reviews_controller.rb
@@ -2,4 +2,10 @@ class CardReviewsController < ApplicationController
   def new
     @card = Card.review_ready.first
   end
+
+  private
+
+  def card_params
+    params.require(:card).permit(:translated_text)
+  end
 end
